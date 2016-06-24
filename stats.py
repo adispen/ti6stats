336cf6e67b90f2f56ca58879414f71c01117041b
Aedan Dispenza <aedan.dispenza@gmail.com>
2016-06-24 11:16:07 -0400

    db working

--- stats.py
+++ stats.py
@@ -3,7 +3,37 @@ import sys
 import time
 import urllib
 from datetime import datetime
+from pymongo import MongoClient
 
+client = MongoClient()
+db = client.stats
+
+def insertStream(now, viewers, stream, gameName, streamTitle, rank):
+	exists = db.streams.find({"name" : stream})
+	if exists.count() == 0:
+		db.streams.insert_one(
+			{
+				"name": stream,
+				"stats": [
+					{
+						"time": now,
+						"viewers": viewers,
+						"title": streamTitle,
+						"game": gameName,
+						"rank": rank
+					}
+				]
+			}
+		)
+	else:
+		newStat = {
+				"time": now,
+				"viewers": viewers,
+				"title": streamTitle,
+				"game": gameName,
+				"rank": rank
+				}
+		db.streams.update_one({"name" : stream}, {"$push" : {"stats" : newStat}})
 
 def getTop():
 	url = "https://api.twitch.tv/kraken/streams?game=dota%202"
@@ -16,14 +46,19 @@ def getTop():
 		else:
 			nowFull = datetime.now()
 			now = nowFull.strftime('%Y-%m-%d %H:%M:%S')
-			#for i, streamObj in enumerate(content['streams']):
-			i = 0
-			viewers = content['streams'][i]['viewers']
-			stream = content['streams'][i]['channel']['display_name']
-			gameName = content['streams'][i]['game']
-			streamTitle = content['streams'][i]['channel']['status']
-			
-			print "#" + str(i+1) + " "  + str(viewers) + " watching " + stream + " play " +  gameName + " at " + now + " " + streamTitle
+			for i, streamObj in enumerate(content['streams']):
+				viewers = content['streams'][i]['viewers']
+				stream = content['streams'][i]['channel']['display_name']
+				gameName = content['streams'][i]['game']
+				streamTitle = content['streams'][i]['channel']['status']
+#				saveName = "images/" + str(stream) + str(now) + ".jpg"
+#				image = urllib.URLopener()
+#				image.retrieve(content['streams'][i]['preview']['large'], saveName)
+				insertStream(now, viewers, stream, gameName, streamTitle, i+1)
+				print "#" + str(i+1) + " "  + str(viewers) + " watching " + stream + " play " +  gameName + " at " + now + " " + streamTitle
 		time.sleep(60)
 
-getTop()
+def main():
+	getTop()
+
+main()
